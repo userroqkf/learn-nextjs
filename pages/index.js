91f57931bc66426eaafb4b515a758a7dca3fba9e
John Kim <96427720+userkimjh@users.noreply.github.com>
2023-07-18 20:03:39 +0800

    fixed typo

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';index
+import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
